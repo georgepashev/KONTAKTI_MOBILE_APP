b966131cf217ef8f2f43c85f4071806938176bb6
George Pashev <georgepashev@uni-plovdiv.bg>
2025-10-07 12:29:45 +0300

    Created .NET MVC Project
    Created .NET MVC Project

---
+++ C_SHARP_MVC/Kontakti/Kontakti/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kontakti.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
